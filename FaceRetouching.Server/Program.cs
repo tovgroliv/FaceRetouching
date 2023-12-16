2980f9cf280f16a50785b3384950531f88b751c7
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-12-16 20:07:23 +0300

    add server

---
+++ FaceRetouching.Server/Program.cs
@@ -0,0 +1,12 @@
+using FaceRetouching.Server.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+app.MapGrpcService<PluginsService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ FaceRetouching.Server/Services/PluginsService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace FaceRetouching.Server.Services;
+
+public class PluginsService : Plugins.PluginsBase
+{
+	public override Task<ListReply> List(ListRequest request, ServerCallContext context)
+	{
+		return base.List(request, context);
+	}
+
+	public override Task<LoadReply> Load(LoadRequest request, ServerCallContext context)
+	{
+		return base.Load(request, context);
+	}
+
+	public override Task<UploadReply> Upload(UploadRequest request, ServerCallContext context)
+	{
+		return base.Upload(request, context);
+	}
+}
