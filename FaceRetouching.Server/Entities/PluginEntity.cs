1631bd8affb8577fb08f20ec2b758e5b9d17645c
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-12-16 20:41:01 +0300

    update entity

--- FaceRetouching.Server/Entities/PluginEntity.cs
+++ FaceRetouching.Server/Entities/PluginEntity.cs
@@ -5,4 +5,5 @@ public class PluginEntity
 	public Guid Id { get; set; }
 	public string Name { get; set; } = "";
 	public string Description { get; set; } = "";
+	public DateTime LastUpdate { get; set; } = DateTime.Now;
 }
